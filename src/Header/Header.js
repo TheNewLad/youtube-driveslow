422ceb88fee56fde4a762d1f446e69f07bee42dd
Karim Dallas-Newell <karim@devslow.tech>
2018-07-14 22:40:06 -0400

    Add gradient to hero

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = (props) => {
     return (
-        <section className="hero is-dark">
+        <section className="hero is-dark is-bold">
             <div className="hero-body">
                 <div className="container">
                     <h1 className="title has-text-centered is-size-1">{props.author}</h1>
